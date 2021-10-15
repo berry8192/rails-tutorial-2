041b169c7d823923c3dd0ffc99ed6eeeb0875d5e
berry8192 <berryrt8192.gmail.com>
2021-10-15 18:45:49 +0900

    user email バリデーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211008091838) do
+ActiveRecord::Schema.define(version: 20211015092256) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
